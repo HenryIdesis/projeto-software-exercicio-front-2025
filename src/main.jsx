9cbb1d70a8cd92657168385eaa68fb630acb3bc9
HenryIdesis <henryi@al.insper.edu.br>
2025-11-10 00:12:35 -0300

    clientid

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Auth0Provider } from '@auth0/auth0-react'
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
       domain="dev-z84f1bu1dhj686n4.us.auth0.com"
-      clientId="69114df476f0ae4d5025e1a5"
+      clientId="6Z3DLJOALa1qqdaKqN1zL2djZsJZ4UPB"
       authorizationParams={{
         audience: "https://dev-z84f1bu1dhj686n4.us.auth0.com/api/v2/",
         redirect_uri: window.location.origin
