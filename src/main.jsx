212130b2ffd35a74f824284e26e9722cc2cf3d62
HenryIdesis <henryi@al.insper.edu.br>
2025-11-10 11:04:50 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import App from './App.jsx'
 import { Auth0Provider } from '@auth0/auth0-react'
 
 
+const redirectUri =
+  import.meta.env.MODE === 'production'
+    ? 'https://projeto-software-exercicio-front-20-two.vercel.app'
+    : window.location.origin;
+
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
       domain="dev-z84f1bu1dhj686n4.us.auth0.com"
       clientId="6Z3DLJOALa1qqdaKqN1zL2djZsJZ4UPB"
       authorizationParams={{
         audience: "https://viagens-api",
-        redirect_uri: window.location.origin
+        redirect_uri: redirectUri
       }}
     >
     <App />
